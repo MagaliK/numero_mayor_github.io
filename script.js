cd1b679c363935b12f1b982ef73e114b5d9eedea
Mirian Magalí Kostinchok Melgarejo <57379255+MagaliK@users.noreply.github.com>
2023-09-11 20:13:59 -0400

    Add files via upload
    commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let num1 = 1;
+let num2 = 2;
+let num3 = 3;
+let numeroMayor;
+
+
+if(num1>num2){
+    numeroMayor=num1;
+    console.log("El numero mayor es: "+numeroMayor)
+}else if(num2>num3){
+    numeroMayor=num2;
+    console.log("El numero mayor es: "+numeroMayor)
+}else {
+    numeroMayor=num3
+    console.log("El numero mayor es: "+numeroMayor)
+}
+
+
+// let may = mayor(num1, num2, num3);
+
+// console.log("El número mayor es: " + mayor);
